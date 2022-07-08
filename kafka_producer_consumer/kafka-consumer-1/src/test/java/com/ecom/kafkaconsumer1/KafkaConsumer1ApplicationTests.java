f10aa61664b0ce748cb6b1d302e7fe236f0758e8
SriramMu <mukesh.sriram@hp.com>
2022-07-08 17:33:52 -0400

    Added Kafka procuder and consumer project

---
+++ kafka_producer_consumer/kafka-consumer-1/src/test/java/com/ecom/kafkaconsumer1/KafkaConsumer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.kafkaconsumer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
